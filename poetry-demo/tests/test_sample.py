4a9953d2791b96554fe1ed65be75f9f968e2c90f
yudai0731 <yudai40018@gmail.com>
2024-02-20 12:28:39 +0900

    #14 remove poetry-demo

--- poetry-demo/tests/test_sample.py
+++
@@ -1,7 +0,0 @@
-from sample import sample_print
-
-def test_sample(capsys):
-    # run "poetry run pytest" on parent dir 
-    sample_print()
-    out, _ = capsys.readouterr()
-    assert out == "sample!\n"
