dad16ec4032db131167421cdea3db7d22b2c56e0
yudai0731 <yudai40018@gmail.com>
2024-02-25 12:33:01 +0900

    #15 modify use classmethod

--- src/use_classmethod.py
+++ src/use_classmethod.py
@@ -4,14 +4,28 @@ from threading import Thread
 from typing import Generator, Iterable, Optional
 
 
-class InputData:
-    """Common class of input data"""
+class GenericInputData:
+    """Generic class of InputData"""
 
-    def read(self):
+    def read(self) -> str:
+        """Read the inpu data.
+
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def generate_inputs(cls, config: dict[str, str]) -> Iterable["GenericInputData"]:
+        """Generate input data instances based on the given configuration.
+
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+        """
         raise NotImplementedError
 
 
-class PathInputData(InputData):
+class PathInputData(GenericInputData):
     """Concrete class of InputData"""
 
     def __init__(self, path: str) -> None:
@@ -19,30 +33,80 @@ class PathInputData(InputData):
         self.path = path
 
     def read(self) -> str:
-        """read self.path's file
+        """Read the content of the file specified by self.path.
 
         Returns:
-            str: content of file
+            str: content of the file.
         """
         with open(self.path) as f:
             return f.read()
 
+    @classmethod
+    def generate_inputs(cls, config: dict[str, str]) -> Iterable["PathInputData"]:
+        """Generate PathInputData instances based on the given configuration.
 
-class Worker:
+        Yields:
+            Iterable[PathInputData]: Instances of PathInputData.
+        """
+        data_dir = config["data_dir"]
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+
+class GenericWorker:
     """Worker of processing input data"""
 
-    def __init__(self, input_data: InputData) -> None:
+    def __init__(self, input_data: GenericInputData) -> None:
         self.input_data = input_data
-        self.result = None
+        self.result = 0
 
     def map(self) -> None:
+        """Perform the mapping step of processing.
+
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+        """
         raise NotImplementedError
 
-    def reduce(self, other: "Worker") -> None:
+    def reduce(self, other: "GenericWorker") -> None:
+        """Perform the reduction step of processing.
+
+        Args:
+            other (GenericWorker): Another worker instance to be reduced with.
+
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+        """
         raise NotImplementedError
 
+    @classmethod
+    def create_workers(
+        cls, input_class: type[GenericInputData], config: dict[str, str]
+    ) -> list["GenericWorker"]:
+        """Create a list of workers based on the provided input class and
+        configuration.
 
-class LineCountWorker(Worker):
+        Args:
+            input_class (GenericInputData): The input data class to be used
+            for generating inputs.
+            config (Dict[str, str]): Configuration containing data directory
+            path.
+
+        Returns:
+            List[GenericWorker]: A list of worker instances.
+
+        Raises:
+            NotImplementedError: If the input class does not support
+            generate_inputs method.
+        """
+        workers = []
+        # cls()で新しいGenericWorkerインスタンスを作る
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+
+
+class LineCountWorker(GenericWorker):
     """Concrete class of Worker"""
 
     def map(self) -> None:
@@ -67,17 +131,19 @@ def generate_inputs(data_dir: str) -> Generator[PathInputData, None, None]:
         data_dir (str): dir path
 
     Yields:
-        Generator[PathInputData, None, None]: PathInputData object of each file content
+        Generator[PathInputData, None, None]: PathInputData object of each
+        file content
     """
     for name in os.listdir(data_dir):
         yield PathInputData(os.path.join(data_dir, name))
 
 
-def create_workers(input_list: Iterable[InputData]) -> list[LineCountWorker]:
+def create_workers(input_list: Iterable[GenericInputData]) -> list[LineCountWorker]:
     """Create list of workers
 
     Args:
-        input_list (Iterable[InputData]): iterable object of InputData instance
+        input_list (Iterable[GenericInputData]): iterable object of
+        GenericInputData instance
 
     Returns:
         list[LineCountWorker]: list of LineCountWorker instance
@@ -88,11 +154,11 @@ def create_workers(input_list: Iterable[InputData]) -> list[LineCountWorker]:
     return workers
 
 
-def execute(workers: list[LineCountWorker]) -> Optional[int]:
+def execute(workers: list[GenericWorker]) -> Optional[int]:
     """Execute MapReduce model
 
     Args:
-        workers (list[LineCountWorker]): Workers to execute
+        workers (list[GenericWorker]): Workers to execute
 
     Returns:
         Optional[int]: Total count of lines in files
@@ -114,9 +180,30 @@ def execute(workers: list[LineCountWorker]) -> Optional[int]:
     return first.result
 
 
-def mapreduce(data_dir: str) -> Optional[int]:
-    inputs = generate_inputs(data_dir)
-    workers = create_workers(inputs)
+def mapreduce(
+    worker_class: type[GenericWorker],
+    input_class: type[GenericInputData],
+    config: dict[str, str],
+) -> Optional[int]:
+    """Execute the MapReduce process using the provided worker class, input
+    class, and configuration.
+
+    Args:
+        worker_class (type[GenericWorker]): The worker class responsible for
+        processing input data.
+        input_class (type[GenericInputData]): The input data class to be used for
+        generating inputs.
+        config (dict[str, str]): Configuration containing data directory path.
+
+    Returns:
+        Optional[int]: The total count of lines in the files processed by the
+        MapReduce process, or None if the process fails.
+
+    Raises:
+        NotImplementedError: If the worker class or input class does not
+        support the required methods.
+    """
+    workers = worker_class.create_workers(input_class, config)
     return execute(workers)
 
 
@@ -139,5 +226,7 @@ if __name__ == "__main__":
     # Reduceステップ : Mapステップの処理結果を集計して最終的な出力を得る.
     tmpdir = "test_inputs"
     write_test_files(tmpdir)
-    result = mapreduce(tmpdir)
+
+    config = {"data_dir": tmpdir}
+    result = mapreduce(LineCountWorker, PathInputData, config)
     print(f"There are {result} lines")
